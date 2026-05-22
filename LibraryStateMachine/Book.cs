f17581a847d742fdf779e0f0420cb10faa2daa5a
Tamir Yamaleyev <tamirka8@gmail.com>
2026-05-22 17:19:37 +0300

    Add project files.

---
+++ LibraryStateMachine/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryStateMachine
+{
+    internal class Book
+    {
+
+    }
+}
