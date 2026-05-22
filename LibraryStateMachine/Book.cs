7ee94147c2b297174562ee48f50fb977c5b46a13
Tamir Yamaleyev <tamirka8@gmail.com>
2026-05-22 17:53:40 +0300

    Created Book and Library classes

--- LibraryStateMachine/Book.cs
+++ LibraryStateMachine/Book.cs
@@ -8,6 +8,23 @@ namespace LibraryStateMachine
 {
     internal class Book
     {
+        private State state;
+        private string renter;
+        private string bookName;
 
+        public Book(string renter)
+        {
+            this.renter = renter;
+            state = new AvailableState();
+        }
+
+        public string Renter => renter;
+        public string BookName => bookName;
+
+        public State State
+        {
+            get { return state; }
+            set { state = value; }
+        }
     }
 }
---
+++ LibraryStateMachine/Library.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryStateMachine
+{
+    internal class Library
+    {
+        private List<Book> books;
+
+        public Library(params Book[] initialBooks)
+        {
+            books = [.. initialBooks];
+        } 
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+            Console.WriteLine($"Added {book.BookName} to Library.");
+        }
+
+        public void RemoveBook(Book book)
+        {
+            books.Remove(book);
+            Console.WriteLine($"Removed {book.BookName} from Library.");
+        }
+
+        public void RentBook(Book book, int days)
+        {
+            book.State.Rent(days);
+            Console.WriteLine($"Renting {book.BookName} for {days} days");
+        }
+
+        public void ReturnBook(Book book)
+        {
+            book.State.Return();
+            Console.WriteLine($"Returning {book.BookName}");
+        }
+
+        public void SetOverdue(Book book)
+        {
+            book.State.SetOverdue();
+            Console.WriteLine($"{book.BookName} is now overdue");
+        }
+    }
+}
