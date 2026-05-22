5bfefd0d697637168e62e1bfa9ae0f0f9075d8b4
Tamir Yamaleyev <tamirka8@gmail.com>
2026-05-22 19:26:22 +0300

    Added small simulation in Main

--- LibraryStateMachine/Program.cs
+++ LibraryStateMachine/Program.cs
@@ -1,12 +1,17 @@
 ﻿using LibraryStateMachine;
 
-Book[] bookDatabase =
-{
-    new Book("Attack on Titan"),
-    new Book("A Song of Fire and Ice"),
-    new Book("Avatar The Last Airbender"),
-    new Book("World Trigger"),
-};
+Book book1 = new Book("Attack on Titan");
+Book book2 = new Book("A Song of Fire and Ice");
+Book book3 = new Book("Avatar The Last Airbender");
+Book book4 = new Book("World Trigger");
+
+Book[] bookDatabase = { book1, book2, book3, book4 };
 
 Library library = new Library(bookDatabase);
 
+library.ShowAvailableBooks();
+
+library.RentBook(book1, 5);
+library.RentBook(book4, 14);
+
+library.ShowAvailableBooks();
