2c86b543a54fd9766a77f2701ac5ba0d5532e9ac
dazuki12 <alisaborzenko@gmail.com>
2026-05-22 22:24:52 +0700

    added abstract states

---
+++ LibraryStateMachine/Rented.cs
@@ -0,0 +1,29 @@
+﻿namespace LibraryStateMachine;
+
+public class Rented : State
+{
+    public override void ShowState()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Rent()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Overdue()
+    {
+        Console.WriteLine("book is overdue");
+    }
+
+    public override void PayFine()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Return()
+    {
+        Console.WriteLine("book is returned");
+    }
+}
